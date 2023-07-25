7d9aeb9be1c4907dfc87260ad3b3bf10ed77ea9b
Ali <AliNazarii7394@gmail.com>
2023-07-25 14:59:44 +0330

    Hero canves created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { Suspense } from "react";
 import styled from "styled-components";
 import Navbar from "./Navbar";
+import { Canvas } from "@react-three/fiber";
+import Cube from "./Cube";
+import { MeshDistortMaterial, OrbitControls, Sphere } from "@react-three/drei";
 
 const Section = styled.div`
   height: 100vh;
@@ -10,53 +13,73 @@ const Section = styled.div`
   align-items: center;
   justify-content: space-between;
 
-  /* @media only screen and (max-width: 768px) {
+  @media only screen and (max-width: 768px) {
     height: 200vh;
-  } */
+  }
 `;
 
 const Container = styled.div`
-  height: 100vh;
+  height: 100%;
   scroll-snap-align: center;
   width: 1400px;
   display: flex;
   justify-content: space-between;
 
-  /* @media only screen and (max-width: 768px) {
+  @media only screen and (max-width: 768px) {
     width: 100%;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-  } */
+  }
 `;
+
 const Left = styled.div`
   flex: 2;
   display: flex;
   flex-direction: column;
   justify-content: center;
   gap: 20px;
+
+  @media only screen and (max-width: 768px) {
+    flex: 1;
+    align-items: center;
+  }
 `;
+
 const Title = styled.h1`
   font-size: 74px;
+
+  @media only screen and (max-width: 768px) {
+    text-align: center;
+  }
 `;
+
 const WhatWeDo = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
 `;
+
 const Line = styled.img`
   height: 5px;
 `;
+
 const Subtitle = styled.h2`
   color: #da4ea2;
 `;
+
 const Desc = styled.p`
   font-size: 24px;
   color: lightgray;
+  @media only screen and (max-width: 768px) {
+    padding: 20px;
+    text-align: center;
+  }
 `;
+
 const Button = styled.button`
   background-color: #da4ea2;
-  color: #fff;
+  color: white;
   font-weight: 500;
   width: 100px;
   padding: 10px;
@@ -68,7 +91,12 @@ const Button = styled.button`
 const Right = styled.div`
   flex: 3;
   position: relative;
+  @media only screen and (max-width: 768px) {
+    flex: 1;
+    width: 100%;
+  }
 `;
+
 const Img = styled.img`
   width: 800px;
   height: 600px;
@@ -108,6 +136,14 @@ export const Hero = () => {
           <Button>Learn More</Button>
         </Left>
         <Right>
+          <Canvas>
+            <OrbitControls enableZoom={false} />
+            <ambientLight intensity={1} />
+            <directionalLight position={[3, 2, 1]} />
+            <Sphere args={[1, 100, 200]} scale={3}>
+              <MeshDistortMaterial color='#3d1c56' attach='material' distort={0.5} speed={2} />
+            </Sphere>
+          </Canvas>
           <Img src='./img/moon.png' />
         </Right>
       </Container>
